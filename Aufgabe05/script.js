f3f8349862006d6388029c71d3f9379174576210
Malte <malte.lukas.huebel@hs-furtwangen.de>
2020-06-03 13:13:40 +0200

    5

--- Aufgabe05/script.js
+++ Aufgabe05/script.js
@@ -1,66 +1,50 @@
 "use strict";
-let tasteninstrumente = [
-    { name: "Keyboard", url: "bilder/keyboard.jpg", beschreibung: "<p>Keyboard mit Tasten</p>", preis: "<hr><p>150€</p>" },
-    { name: "Digitales Piano", url: "bilder/digital_piano.jpg", beschreibung: "<p>Digitales Piano mit Tasten</p>", preis: "<hr><p>200€</p>" },
-    { name: "Stage Piano", url: "bilder/stage_piano.jpg", beschreibung: "<p>Stage Pino mit Tasten</p>", preis: "<hr><p>250€</p>" },
-    { name: "Elektrische Orgel", url: "bilder/elektrische_orgel.jpg", beschreibung: "<p>Elektrische Orgel mit Tasten</p>", preis: "<hr><p>300€</p>" },
-    { name: "Klavier", url: "bilder/klavier.jpg", beschreibung: "<p>Klavier mit Tasten</p>", preis: "<hr><p>220€</p>" },
-    { name: "Akkordeon", url: "bilder/akkordeon.jpg", beschreibung: "<p>Akkordeon<p>", preis: "<hr><p>200€</p>" },
-    { name: "Flügel", url: "bilder/fluegel.jpg", beschreibung: "<p>Flügel mit Tasten</p>", preis: "<hr><p>500€</p>" }
-];
-let blasinstrumente = [
-    { name: "Trompete", url: "bilder/trompete.jpg", beschreibung: "<p>Trompete zum Spielen</p>", preis: "<hr><p>80€</p>" },
-    { name: "Saxophon", url: "bilder/saxophon.jpg", beschreibung: "<p>Saxophon zum Spielen</p>", preis: "<hr><p>120€</p>" },
-    { name: "Klarinette", url: "bilder/klarinette.jpg", beschreibung: "<p>Klarinette zum Spielen</p>", preis: "<hr><p>100€</p>" },
-    { name: "Horn", url: "bilder/horn.jpg", beschreibung: "<p>Super Horn zum Spielen</p>", preis: "<hr><p>100€</p>" },
-    { name: "Posaune", url: "bilder/posaune.jpg", beschreibung: "<p>Posaune zum Spielen</p>", preis: "<hr><p>150€</p>" },
-    { name: "Querflöte", url: "bilder/querfloete.jpg", beschreibung: "<p>Querflöte zum Spielen</p>", preis: "<hr><p>80€</p>" },
-    { name: "Trompete", url: "bilder/trompete.jpg", beschreibung: "<p>Trompete zum Spielen</p>", preis: "<hr><p>80€</p>" }
-];
-for (let index = 0; index < tasteninstrumente.length; index++) {
-    let newDiv = document.createElement("div");
-    newDiv.id = "at" + index;
-    document.getElementById("ti_artikel")?.appendChild(newDiv);
-    newDiv.setAttribute("class", "tasteninstrumente");
-    let newTitel = document.createElement("p");
-    newTitel.innerHTML = tasteninstrumente[index].name;
-    document.getElementById("at" + index)?.appendChild(newTitel);
-    let newPic = document.createElement("img");
-    newPic.setAttribute("src", tasteninstrumente[index].url);
-    document.getElementById("at" + index)?.appendChild(newPic);
-    let newBeschreibung = document.createElement("p");
-    newBeschreibung.innerHTML = tasteninstrumente[index].beschreibung;
-    document.getElementById("at" + index)?.appendChild(newBeschreibung);
-    let newPreis = document.createElement("p");
-    newPreis.innerHTML = tasteninstrumente[index].preis;
-    document.getElementById("at" + index)?.appendChild(newPreis);
-    let newButton = document.createElement("input");
-    newButton.setAttribute("class", "in_den_einkaufswagen");
-    document.getElementById("at" + index)?.appendChild(newButton);
-    newButton.value = "In den Einkaufswagen";
-    newButton.type = "submit";
-}
-for (let index = 0; index < blasinstrumente.length; index++) {
-    let newDiv = document.createElement("div");
-    newDiv.id = "ab" + index;
-    document.getElementById("bi_artikel")?.appendChild(newDiv);
-    newDiv.setAttribute("class", "blasinstrumente");
-    let newTitel = document.createElement("p");
-    newTitel.innerHTML = blasinstrumente[index].name;
-    document.getElementById("ab" + index)?.appendChild(newTitel);
-    let newPic = document.createElement("img");
-    newPic.setAttribute("src", blasinstrumente[index].url);
-    document.getElementById("ab" + index)?.appendChild(newPic);
-    let newBeschreibung = document.createElement("p");
-    newBeschreibung.innerHTML = blasinstrumente[index].beschreibung;
-    document.getElementById("ab" + index)?.appendChild(newBeschreibung);
-    let newPreis = document.createElement("p");
-    newPreis.innerHTML = blasinstrumente[index].preis;
-    document.getElementById("ab" + index)?.appendChild(newPreis);
-    let newButton = document.createElement("input");
-    newButton.setAttribute("class", "in_den_einkaufswagen");
-    document.getElementById("ab" + index)?.appendChild(newButton);
-    newButton.value = "In den Einkaufswagen";
-    newButton.type = "submit";
-}
+var Aufgabe05;
+(function (Aufgabe05) {
+    let instrumente = [
+        { name: "Keyboard", url: "bilder/keyboard.jpg", beschreibung: "<p>Keyboard mit Tasten</p>", preis: 150, kategorie: 0 },
+        { name: "Digitales Piano", url: "bilder/digital_piano.jpg", beschreibung: "<p>Digitales Piano mit Tasten</p>", preis: 200, kategorie: 0 },
+        { name: "Stage Piano", url: "bilder/stage_piano.jpg", beschreibung: "<p>Stage Pino mit Tasten</p>", preis: 250, kategorie: 0 },
+        { name: "Elektrische Orgel", url: "bilder/elektrische_orgel.jpg", beschreibung: "<p>Elektrische Orgel mit Tasten</p>", preis: 300, kategorie: 0 },
+        { name: "Klavier", url: "bilder/klavier.jpg", beschreibung: "<p>Klavier mit Tasten</p>", preis: 220, kategorie: 0 },
+        { name: "Akkordeon", url: "bilder/akkordeon.jpg", beschreibung: "<p>Akkordeon<p>", preis: 200, kategorie: 0 },
+        { name: "Flügel", url: "bilder/fluegel.jpg", beschreibung: "<p>Flügel mit Tasten</p>", preis: 500, kategorie: 0 },
+        { name: "Trompete", url: "bilder/trompete.jpg", beschreibung: "<p>Trompete zum Spielen</p>", preis: 80, kategorie: 1 },
+        { name: "Saxophon", url: "bilder/saxophon.jpg", beschreibung: "<p>Saxophon zum Spielen</p>", preis: 120, kategorie: 1 },
+        { name: "Klarinette", url: "bilder/klarinette.jpg", beschreibung: "<p>Klarinette zum Spielen</p>", preis: 100, kategorie: 1 },
+        { name: "Horn", url: "bilder/horn.jpg", beschreibung: "<p>Super Horn zum Spielen</p>", preis: 100, kategorie: 1 },
+        { name: "Posaune", url: "bilder/posaune.jpg", beschreibung: "<p>Posaune zum Spielen</p>", preis: 150, kategorie: 1 },
+        { name: "Querflöte", url: "bilder/querfloete.jpg", beschreibung: "<p>Querflöte zum Spielen</p>", preis: 80, kategorie: 1 },
+        { name: "Trompete", url: "bilder/trompete.jpg", beschreibung: "<p>Trompete zum Spielen</p>", preis: 80, kategorie: 1 }
+    ];
+    for (let index = 0; index < instrumente.length; index++) {
+        let newDiv = document.createElement("div");
+        newDiv.id = "at" + index;
+        if (instrumente[index].kategorie == 0) {
+            document.getElementById("ti_artikel")?.appendChild(newDiv);
+            newDiv.setAttribute("class", "tasteninstrumente");
+        }
+        if (instrumente[index].kategorie == 1) {
+            document.getElementById("bi_artikel")?.appendChild(newDiv);
+            newDiv.setAttribute("class", "blasinstrumente");
+        }
+        let newTitel = document.createElement("p");
+        newTitel.innerHTML = instrumente[index].name;
+        document.getElementById("at" + index)?.appendChild(newTitel);
+        let newPic = document.createElement("img");
+        newPic.setAttribute("src", instrumente[index].url);
+        document.getElementById("at" + index)?.appendChild(newPic);
+        let newBeschreibung = document.createElement("p");
+        newBeschreibung.innerHTML = instrumente[index].beschreibung;
+        document.getElementById("at" + index)?.appendChild(newBeschreibung);
+        let newPreis = document.createElement("p");
+        newPreis.innerHTML = "<hr><p>" + instrumente[index].preis + " €</p></hr>";
+        document.getElementById("at" + index)?.appendChild(newPreis);
+        let newButton = document.createElement("input");
+        newButton.setAttribute("class", "in_den_einkaufswagen");
+        document.getElementById("at" + index)?.appendChild(newButton);
+        newButton.value = "In den Einkaufswagen";
+        newButton.type = "submit";
+    }
+})(Aufgabe05 || (Aufgabe05 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe05/script.ts
+++ Aufgabe05/script.ts
@@ -1,51 +1,62 @@
+namespace Aufgabe05 {
 interface Artikel {
     name: string;
     url: string;
     beschreibung: string;
-    preis: string;
+    preis: number;
+    kategorie: number;
 }
 
-let tasteninstrumente: Artikel[] = [
-    {name: "Keyboard", url: "bilder/keyboard.jpg", beschreibung: "<p>Keyboard mit Tasten</p>", preis: "<hr><p>150€</p>"},
-    {name: "Digitales Piano", url: "bilder/digital_piano.jpg", beschreibung: "<p>Digitales Piano mit Tasten</p>", preis: "<hr><p>200€</p>"},
-    {name: "Stage Piano", url: "bilder/stage_piano.jpg", beschreibung: "<p>Stage Pino mit Tasten</p>", preis: "<hr><p>250€</p>"},
-    {name: "Elektrische Orgel", url: "bilder/elektrische_orgel.jpg", beschreibung: "<p>Elektrische Orgel mit Tasten</p>", preis: "<hr><p>300€</p>"},
-    {name: "Klavier", url: "bilder/klavier.jpg", beschreibung: "<p>Klavier mit Tasten</p>", preis: "<hr><p>220€</p>"},
-    {name: "Akkordeon", url: "bilder/akkordeon.jpg", beschreibung: "<p>Akkordeon<p>", preis: "<hr><p>200€</p>"},
-    {name: "Flügel", url: "bilder/fluegel.jpg", beschreibung: "<p>Flügel mit Tasten</p>", preis: "<hr><p>500€</p>"}
+let instrumente: Artikel[] = [
+    {name: "Keyboard", url: "bilder/keyboard.jpg", beschreibung: "<p>Keyboard mit Tasten</p>", preis: 150, kategorie: 0},
+    {name: "Digitales Piano", url: "bilder/digital_piano.jpg", beschreibung: "<p>Digitales Piano mit Tasten</p>", preis: 200, kategorie: 0},
+    {name: "Stage Piano", url: "bilder/stage_piano.jpg", beschreibung: "<p>Stage Pino mit Tasten</p>", preis: 250, kategorie: 0},
+    {name: "Elektrische Orgel", url: "bilder/elektrische_orgel.jpg", beschreibung: "<p>Elektrische Orgel mit Tasten</p>", preis: 300, kategorie: 0},
+    {name: "Klavier", url: "bilder/klavier.jpg", beschreibung: "<p>Klavier mit Tasten</p>", preis: 220, kategorie: 0},
+    {name: "Akkordeon", url: "bilder/akkordeon.jpg", beschreibung: "<p>Akkordeon<p>", preis: 200, kategorie: 0},
+    {name: "Flügel", url: "bilder/fluegel.jpg", beschreibung: "<p>Flügel mit Tasten</p>", preis: 500, kategorie: 0},
+
+    {name: "Trompete", url: "bilder/trompete.jpg", beschreibung: "<p>Trompete zum Spielen</p>", preis: 80, kategorie: 1},
+    {name: "Saxophon", url: "bilder/saxophon.jpg", beschreibung: "<p>Saxophon zum Spielen</p>", preis: 120, kategorie: 1},
+    {name: "Klarinette", url: "bilder/klarinette.jpg", beschreibung: "<p>Klarinette zum Spielen</p>", preis: 100, kategorie: 1},
+    {name: "Horn", url: "bilder/horn.jpg", beschreibung: "<p>Super Horn zum Spielen</p>", preis: 100, kategorie: 1},
+    {name: "Posaune", url: "bilder/posaune.jpg", beschreibung: "<p>Posaune zum Spielen</p>", preis: 150, kategorie: 1},
+    {name: "Querflöte", url: "bilder/querfloete.jpg", beschreibung: "<p>Querflöte zum Spielen</p>", preis: 80, kategorie: 1},
+    {name: "Trompete", url: "bilder/trompete.jpg", beschreibung: "<p>Trompete zum Spielen</p>", preis: 80, kategorie: 1}
 ];
 
-let blasinstrumente: Artikel[] = [
-    {name: "Trompete", url: "bilder/trompete.jpg", beschreibung: "<p>Trompete zum Spielen</p>", preis: "<hr><p>80€</p>"},
-    {name: "Saxophon", url: "bilder/saxophon.jpg", beschreibung: "<p>Saxophon zum Spielen</p>", preis: "<hr><p>120€</p>"},
-    {name: "Klarinette", url: "bilder/klarinette.jpg", beschreibung: "<p>Klarinette zum Spielen</p>", preis: "<hr><p>100€</p>"},
-    {name: "Horn", url: "bilder/horn.jpg", beschreibung: "<p>Super Horn zum Spielen</p>", preis: "<hr><p>100€</p>"},
-    {name: "Posaune", url: "bilder/posaune.jpg", beschreibung: "<p>Posaune zum Spielen</p>", preis: "<hr><p>150€</p>"},
-    {name: "Querflöte", url: "bilder/querfloete.jpg", beschreibung: "<p>Querflöte zum Spielen</p>", preis: "<hr><p>80€</p>"},
-    {name: "Trompete", url: "bilder/trompete.jpg", beschreibung: "<p>Trompete zum Spielen</p>", preis: "<hr><p>80€</p>"}
-];
-
-for (let index: number = 0; index < tasteninstrumente.length; index++) {
+for (let index: number = 0; index < instrumente.length; index++) {
     
     let newDiv: HTMLElement = document.createElement("div");
     newDiv.id = "at" + index;
-    document.getElementById("ti_artikel")?.appendChild(newDiv);
-    newDiv.setAttribute("class", "tasteninstrumente");
+
+    if (instrumente[index].kategorie == 0) {
+        document.getElementById("ti_artikel")?.appendChild(newDiv);
+        newDiv.setAttribute("class", "tasteninstrumente");
+
+    }
+
+    if (instrumente[index].kategorie == 1) {
+        document.getElementById("bi_artikel")?.appendChild(newDiv);
+        newDiv.setAttribute("class", "blasinstrumente");
+
+    }
 
     let newTitel: HTMLElement = document.createElement("p");
-    newTitel.innerHTML = tasteninstrumente[index].name;
+    newTitel.innerHTML = instrumente[index].name;
     document.getElementById("at" + index)?.appendChild(newTitel);
 
     let newPic: HTMLElement = document.createElement("img");
-    newPic.setAttribute("src", tasteninstrumente[index].url);
+    newPic.setAttribute("src", instrumente[index].url);
     document.getElementById("at" + index)?.appendChild(newPic);
 
     let newBeschreibung: HTMLElement = document.createElement("p");
-    newBeschreibung.innerHTML = tasteninstrumente[index].beschreibung;
+    newBeschreibung.innerHTML = instrumente[index].beschreibung;
     document.getElementById("at" + index)?.appendChild(newBeschreibung);
 
     let newPreis: HTMLElement = document.createElement("p");
-    newPreis.innerHTML = tasteninstrumente[index].preis;
+
+    newPreis.innerHTML = "<hr><p>" + instrumente[index].preis + " €</p></hr>";
     document.getElementById("at" + index)?.appendChild(newPreis);
 
     let newButton: HTMLInputElement = document.createElement("input");
@@ -53,38 +64,5 @@ for (let index: number = 0; index < tasteninstrumente.length; index++) {
     document.getElementById("at" + index)?.appendChild(newButton);
     newButton.value = "In den Einkaufswagen";
     newButton.type = "submit";
-
-
 }
-
-for (let index: number = 0; index < blasinstrumente.length; index++) {
-    
-    let newDiv: HTMLElement = document.createElement("div");
-    newDiv.id = "ab" + index;
-    document.getElementById("bi_artikel")?.appendChild(newDiv);
-    newDiv.setAttribute("class", "blasinstrumente");
-
-    let newTitel: HTMLElement = document.createElement("p");
-    newTitel.innerHTML = blasinstrumente[index].name;
-    document.getElementById("ab" + index)?.appendChild(newTitel);
-
-    let newPic: HTMLElement = document.createElement("img");
-    newPic.setAttribute("src", blasinstrumente[index].url);
-    document.getElementById("ab" + index)?.appendChild(newPic);
-
-    let newBeschreibung: HTMLElement = document.createElement("p");
-    newBeschreibung.innerHTML = blasinstrumente[index].beschreibung;
-    document.getElementById("ab" + index)?.appendChild(newBeschreibung);
-
-    let newPreis: HTMLElement = document.createElement("p");
-    newPreis.innerHTML = blasinstrumente[index].preis;
-    document.getElementById("ab" + index)?.appendChild(newPreis);
-
-    let newButton: HTMLInputElement = document.createElement("input");
-    newButton.setAttribute("class", "in_den_einkaufswagen");
-    document.getElementById("ab" + index)?.appendChild(newButton);
-    newButton.value = "In den Einkaufswagen";
-    newButton.type = "submit";
-
 }
-
