85bbdaf19beed36d9c6b925ff09cb2a8d2f248fe
Malte <malte.lukas.huebel@hs-furtwangen.de>
2020-06-20 14:33:03 +0200

    server

--- Aufgabe08neu/antwort.js
+++ Aufgabe08neu/antwort.js
@@ -3,7 +3,7 @@ let knepp = document.getElementById("hammerknopf");
 knepp.addEventListener("click", ausgeben);
 async function ausgeben(_event) {
     let formData = new FormData(document.forms[0]);
-    let url = "https://gis-example.herokuapp.com";
+    let url = "https://gismalteshesh.herokuapp.com/";
     let query = new URLSearchParams(formData);
     url = url + "?" + query.toString();
     await fetch(url);
--- Aufgabe08neu/antwort.ts
+++ Aufgabe08neu/antwort.ts
@@ -3,7 +3,7 @@ knepp.addEventListener("click", ausgeben);
 
 async function ausgeben(_event: Event): Promise<void> {
     let formData: FormData = new FormData(document.forms[0]);
-    let url: string = "https://gis-example.herokuapp.com";
+    let url: string = "https://gismalteshesh.herokuapp.com/";
     let query: URLSearchParams = new URLSearchParams(<any>formData);
     url = url + "?" + query.toString();
     await fetch(url);
