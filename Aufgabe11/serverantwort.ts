ba06b8c19f392bca6898efd347110f4438d1b048
Malte <malte.lukas.huebel@hs-furtwangen.de>
2020-07-12 10:17:02 +0200

    11ende

--- Aufgabe11/serverantwort.ts
+++ Aufgabe11/serverantwort.ts
@@ -5,7 +5,7 @@ namespace Aufgabe11 {
     async function datenbank(_event: Event): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-        let url: string = "//https://gismalteshesh.herokuapp.com";
+        let url: string = "https://gismalteshesh.herokuapp.com";
         url = url + "/datenbank";
         url = url + "?" + query.toString();
         let response: Response = await fetch(url);
@@ -16,7 +16,7 @@ namespace Aufgabe11 {
     async function ausgebenhtml(_event: Event): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-        let url: string = "//https://gismalteshesh.herokuapp.com";
+        let url: string = "https://gismalteshesh.herokuapp.com";
         url = url + "/back";
         url = url + "?" + query.toString();
         let response: Response = await fetch(url);
